ab8e59859615aa3fac21156396081e137f05a161
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:30:58 +0700

    Add getPaymentDetails in PaymentDetailsController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/paymentdetailsservice/controller/PaymentDetailsController.java
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/controller/PaymentDetailsController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.paymentdetailsservice.controller;
 
+import id.my.hendisantika.paymentdetailsservice.dto.PaymentDetailsDto;
+import id.my.hendisantika.paymentdetailsservice.model.PaymentDetails;
 import id.my.hendisantika.paymentdetailsservice.service.PaymentDetailsService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,5 +27,15 @@ public class PaymentDetailsController {
 
     public PaymentDetailsController(PaymentDetailsService paymentDetailsService) {
         this.paymentDetailsService = paymentDetailsService;
+    }
+
+    @GetMapping("/v1/payment-details/{reference}")
+    public ResponseEntity<PaymentDetailsDto> getPaymentDetails(@PathVariable("reference") String reference) {
+        log.debug("Received GET request: {}", reference);
+
+        PaymentDetails paymentDetails = paymentDetailsService.get(reference);
+        log.info("payment details {}", paymentDetails);
+        var paymentDetailsDto = PaymentDetailsDto.fromDomain(paymentDetails);
+        return ResponseEntity.ok().body(paymentDetailsDto);
     }
 }
