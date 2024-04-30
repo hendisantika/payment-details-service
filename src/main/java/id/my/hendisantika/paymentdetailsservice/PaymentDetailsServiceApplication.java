498a3f42c7d8198812ccb8124068c43378636fa0
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:19:13 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/PaymentDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.paymentdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentDetailsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentDetailsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/paymentdetailsservice/PaymentDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.paymentdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDetailsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
