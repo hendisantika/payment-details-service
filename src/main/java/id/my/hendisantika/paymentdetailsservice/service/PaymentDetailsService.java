f334eedd29512a69ce586c30cd92b3ac2a558862
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:28:15 +0700

    Add store in PaymentDetailsService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/paymentdetailsservice/service/PaymentDetailsService.java
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/service/PaymentDetailsService.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.paymentdetailsservice.service;
 
+import id.my.hendisantika.paymentdetailsservice.model.PaymentDetails;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RMapCache;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,17 @@ public class PaymentDetailsService {
 
     private static final long TIME_TO_LIVE = 10;
 
+    private final RMapCache<String, PaymentDetails> cache;
+
+    public PaymentDetailsService(RedissonClient redissonClient) {
+        cache = redissonClient.getMapCache("PaymentDetails");
+    }
+
+    public void store(PaymentDetails paymentDetails) {
+        try {
+            cache.put(paymentDetails.reference(), paymentDetails, TIME_TO_LIVE, TimeUnit.MINUTES);
+        } catch (RuntimeException e) {
+            log.warn("Could not store payment details {}", paymentDetails, e);
+        }
+    }
 }
