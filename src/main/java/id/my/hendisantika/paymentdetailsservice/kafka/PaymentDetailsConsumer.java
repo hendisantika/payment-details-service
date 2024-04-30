ace4b67e442769a964f1ce43d80df7ccf6a38da3
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:29:07 +0700

    Add onMessage in PaymentDetailsConsumer🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/paymentdetailsservice/kafka/PaymentDetailsConsumer.java
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/kafka/PaymentDetailsConsumer.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.paymentdetailsservice.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.paymentdetailsservice.dto.PaymentDetailsDto;
 import id.my.hendisantika.paymentdetailsservice.service.PaymentDetailsService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,4 +27,25 @@ public class PaymentDetailsConsumer {
 
     private final ObjectMapper objectMapper;
     private final PaymentDetailsService paymentDetailsService;
+
+    @KafkaListener(
+            containerFactory = "paymentDetailsKafkaListenerContainerFactory",
+            topics = "payment-details",
+            concurrency = "${kafka.consumer.payment-details.threads}",
+            clientIdPrefix = "payment-details-service-${random.uuid}",
+            autoStartup = "${kafka.consumer.payment-details.autostart}"
+    )
+    public void onMessage(@Payload String payload, Acknowledgment acknowledgment) {
+        acknowledgment.acknowledge();
+
+        try {
+            var paymentMessageDto = objectMapper.readValue(payload, PaymentDetailsDto.class);
+            log.debug("payment details message received: {}", paymentMessageDto);
+
+            var paymentDetails = paymentMessageDto.toModel();
+            paymentDetailsService.store(paymentDetails);
+        } catch (Exception e) {
+            log.warn("Unable to process processing account settings " + payload, e);
+        }
+    }
 }
