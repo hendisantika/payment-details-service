298ff6135a3d77c57efca71a2cb6260053fbdd14
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:26:06 +0700

    Add RepositoryException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/exception/RepositoryException.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.paymentdetailsservice.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : payment-details-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/30/24
+ * Time: 08:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class RepositoryException extends RuntimeException {
+
+    public RepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
