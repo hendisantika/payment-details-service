61c28eb8a24c14817cec3863d9ec257dafa4de11
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:23:39 +0700

    Add paymentDetailsKafkaListenerContainerFactory in PaymentDetailsConfiguration🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/paymentdetailsservice/config/PaymentDetailsConfiguration.java
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/config/PaymentDetailsConfiguration.java
@@ -1,7 +1,15 @@
 package id.my.hendisantika.paymentdetailsservice.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +24,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties({RedisProperties.class, KafkaProperties.class})
 public class PaymentDetailsConfiguration {
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> paymentDetailsKafkaListenerContainerFactory(KafkaProperties kafkaProperties) {
+        var consumerProperties = kafkaProperties.consumer().paymentDetails();
+
+        var props = new HashMap<String, Object>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.bootstrapServers());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerProperties.groupId());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerProperties.autoOffsetReset());
+
+    /*
+    To ensure that messages are processed only once, we can use MANUAL_IMMEDIATE acknowledgment mode.
+    With that being used, we must explicitly acknowledge each message that the listener receives.
+    Otherwise, the message will be redelivered to the consumer.
+     */
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(props, StringDeserializer::new, StringDeserializer::new));
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
+
 }
