ac0cee8696bedb368d820679820cbaed9ad79b19
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:29:54 +0700

    Add RestExceptionHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/controller/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.paymentdetailsservice.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : payment-details-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/30/24
+ * Time: 08:29
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@ControllerAdvice
+@AllArgsConstructor
+public class RestExceptionHandler {
+}
