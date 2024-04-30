8762b3c1c8eb975538e2340a31f90561d3424c2f
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 08:27:45 +0700

    Add PaymentDetailsService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/paymentdetailsservice/service/PaymentDetailsService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.paymentdetailsservice.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : payment-details-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/30/24
+ * Time: 08:27
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+public class PaymentDetailsService {
+
+    private static final long TIME_TO_LIVE = 10;
+
+}
